8f2a287d057ed1092a3410673d3348798017e366
IF2290 <246528729+IF2290@users.noreply.github.com>
2026-07-26 15:38:19 +0530

    Add functionality to calculator (now shows ans)
    I fixed some bugs made the event listeners change the display text content

--- script.js
+++ script.js
@@ -31,9 +31,10 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 };
-let num1;
+let num1 = "";
 let operator;
-let num2;
+let num2 = "";
+let ans = "";
 function operate(){
     if(operator === "+"){
         return add(Number(num1), Number(num2));
@@ -50,3 +51,161 @@ function operate(){
         return "ERROR";
     };
 };
+button0.addEventListener("click", () => {
+    if(operator === undefined){
+         num1 += "0";
+         display.textContent = num1;
+        }else{
+         num2 += "0";
+         display.textContent = num2;
+        };
+});
+button1.addEventListener("click", () => {
+    if(operator === undefined){
+         num1 += "1";
+         display.textContent = num1;
+        }else{
+         num2 += "1";
+         display.textContent = num2;
+        };
+});
+button2.addEventListener("click", () => {
+    if(operator === undefined){
+         num1 += "2";
+         display.textContent = num1;
+        }else{
+         num2 += "2";
+         display.textContent = num2;
+        };
+});
+button3.addEventListener("click", () => {
+    if(operator === undefined){
+         num1 += "3";
+         display.textContent = num1;
+        }else{
+         num2 += "3";
+         display.textContent = num2;
+        };
+});
+button4.addEventListener("click", () => {
+    if(operator === undefined){
+         num1 += "4";
+         display.textContent = num1;
+        }else{
+         num2 += "4";
+         display.textContent = num2;
+        };
+});
+button5.addEventListener("click", () => {
+    if(operator === undefined){
+         num1 += "5";
+         display.textContent = num1;
+        }else{
+         num2 += "5";
+         display.textContent = num2;
+        };
+});
+button6.addEventListener("click", () => {
+    if(operator === undefined){
+         num1 += "6";
+         display.textContent = num1;
+        }else{
+         num2 += "6";
+         display.textContent = num2;
+        };
+});
+button7.addEventListener("click", () => {
+    if(operator === undefined){
+         num1 += "7";
+         display.textContent = num1;
+        }else{
+         num2 += "7";
+         display.textContent = num2;
+        };
+});
+button8.addEventListener("click", () => {
+    if(operator === undefined){
+         num1 += "8";
+         display.textContent = num1;
+        }else{
+         num2 += "8";
+         display.textContent = num2;
+        };
+});
+button9.addEventListener("click", () => {
+    if(operator === undefined){
+         num1 += "9";
+         display.textContent = num1;
+        }else{
+         num2 += "9";
+         display.textContent = num2;
+        };
+});
+
+buttonAdd.addEventListener("click", () => {
+         if(num1 !== "" && num2 !== ""){
+         ans = operate();
+         num1 = ans;
+         num2 = "";
+         display.textContent = num1;
+         operator = "+";
+    }else if(num1 !== "" && num2 === ""){
+        operator = "+";
+    };
+}
+);
+buttonSub.addEventListener("click", () => {
+    if(num1 !== "" && num2 !== ""){
+         ans = operate();
+         num1 = ans;
+         num2 = "";
+         display.textContent = num1;
+         operator = "-";
+    }else if(num1 !== "" && num2 === ""){
+        operator = "-";
+    };
+});
+buttonMult.addEventListener("click", () => {
+    if(num1 !== "" && num2 !== ""){
+         ans = operate();
+         num1 = ans;
+         num2 = "";
+         display.textContent = num1;
+         operator = "*";
+    }else if(num1 !== "" && num2 === ""){
+        operator = "*";
+    };
+});
+buttonDiv.addEventListener("click", () => {
+     if(num1 !== "" && num2 !== ""){
+         ans = operate();
+         num1 = ans;
+         num2 = "";
+         display.textContent = num1;
+         operator = "/";
+    }else if(num1 !== "" && num2 === ""){
+        operator = "/";
+    };
+});
+
+buttonEquals.addEventListener("click", () => {
+    if(num1 !== "" && num2 !== ""){
+        ans = operate();
+        num1 = ans;
+        num2 = "";
+        operator = undefined;
+        display.textContent = num1;
+    };
+});
+buttonClear.addEventListener("click", () => {
+    num1 = "";
+    num2 = "";
+    operator = undefined;
+});
+buttonBackspace.addEventListener("click", () => {
+    if(operator === undefined){
+        num1 = num1.slice(0, -1);
+    }else{
+        num2 = num2.slice(0, -1);
+    };
+});
