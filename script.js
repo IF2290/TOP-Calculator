8f23189b1a34d88455256cae36c7a313a619b0bf
IF2290 <246528729+IF2290@users.noreply.github.com>
2026-07-26 13:35:12 +0530

    Query select all buttons & display in js
    I did this to access them in js.

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 const display = document.querySelector(".display");
+
+const button1 = document.querySelector(".number1");
+const button2 = document.querySelector(".number2");
+const button3 = document.querySelector(".number3");
+const button4 = document.querySelector(".number4");
+const button5 = document.querySelector(".number5");
+const button6 = document.querySelector(".number6");
+const button7 = document.querySelector(".number7");
+const button8 = document.querySelector(".number8");
+const button9 = document.querySelector(".number9");
+const button0 = document.querySelector(".number0");
+
+const buttonAdd = document.querySelector(".add-op");
+const buttonSub = document.querySelector(".subtract-op");
+const buttonMult = document.querySelector(".multiply-op");
+const buttonDiv = document.querySelector(".divide-op");
+const buttonClear = document.querySelector(".clear");
+const buttonBackspace = document.querySelector(".backspace");
+const buttonEquals = document.querySelector(".equals");
+
 function add(num1, num2){
     return num1 + num2;
 };
